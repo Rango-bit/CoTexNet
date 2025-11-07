488e698a56d3443fba3a86debe28f0d432d7bf9c
Rango-bit <69458361+Rango-bit@users.noreply.github.com>
2025-11-07 09:53:49 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,9 +5,10 @@ from model.CoTexNet import CLIPSeg_fintune
 
 if __name__ == '__main__':
 
-    model_name = 'Clipseg'
+    model_name = 'CoTexNet'
 
     args = read_parser('./dataset_config/train_config.yaml')
     model = CLIPSeg_fintune(args.clipseg_hf_api, args.num_classes)
+
 
     train_main(model, model_name, args)
